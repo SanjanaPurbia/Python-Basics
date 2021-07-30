d112b8ac17d765881467330458badab5e412a022
Sanjana Purbia <71418488+SanjanaPurbia@users.noreply.github.com>
2021-07-30 14:34:00 +0530

    Basic

---
+++ function1.py
@@ -0,0 +1,7 @@
+def sentence(name="sona",action="loves",item="cutiee"):
+    print(name,action,item)
+    
+sentence()
+sentence("sanjana","loves","yousss")
+sentence(item="sunflower")
+sentence(item="sunflower",action="likes")
