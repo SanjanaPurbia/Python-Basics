80f5c7c465e6acbe0f3f56fb7b2e726fd8526735
Sanjana Purbia <71418488+SanjanaPurbia@users.noreply.github.com>
2021-08-19 22:42:24 +0530

    Basic

---
+++ tuple1.py
@@ -0,0 +1,6 @@
+#creating a tuple to store mixed value
+mix = (1250,560.90,'IN@20',455,"Udr#Smart",
+       (1101,"sanju",1240),121.345,'''Tech$ US''')
+print(mix)
+for item in mix:
+     print(item) 
