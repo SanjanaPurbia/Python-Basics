e027ef6dbf30cf165009db27ccfbe55003b2a1be
Sanjana Purbia <71418488+SanjanaPurbia@users.noreply.github.com>
2021-09-19 17:13:06 +0530

    Basic

---
+++ lenFunc.py
@@ -0,0 +1,8 @@
+print("What's your name?")
+name=input()
+n=len(name)
+print("enter your age:")
+age=int(input())
+new_age=age+1
+print("Hii!! {} lenght of your name is {}".format(name,n))
+print("you will be {} in a year".format(new_age))
